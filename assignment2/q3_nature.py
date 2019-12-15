44ecbd33757ceeb8a70080e4e828d07377cc0a29
jimzers <adamleesaintfrancis@gmail.com>
2019-12-14 16:07:35 -0800

    attempt 1 of q3

--- assignment2/q3_nature.py
+++ assignment2/q3_nature.py
@@ -53,9 +53,24 @@ class NatureQN(Linear):
 
         """
         ##############################################################
-        ################ YOUR CODE HERE - 10-15 lines ################ 
+        ################ YOUR CODE HERE - 10-15 lines ################
 
-        pass
+        # image height - 84 x 84 x 4 - 4th channel is the alpha channel
+        conv1 = layers.conv2d(inputs=state, kernel_size=(8, 8), num_outputs=32, stride=4, scope=scope, reuse=reuse, name='conv1')
+        relu1 = tf.nn.relu(conv1, name='relu1')
+
+        conv2 = layers.conv2d(inputs=relu1, kernel_size=(4, 4), num_outputs=64, stride=2, scope=scope, reuse=reuse, name='conv2')
+        relu2 = tf.nn.relu(conv2, name='relu2')
+
+        conv3 = layers.conv2d(inputs=relu2, kernel_size=(3, 3), num_outputs=64, stride=1, scope=scope, reuse=reuse,
+                              name='conv3')
+        relu3 = tf.nn.relu(conv3, name='relu3')
+
+        dense1 = layers.dense(inputs=relu3, units=512, scope=scope, reuse=reuse, name='dense1')
+        relu4 = tf.nn.relu(dense1, name='dense1')
+
+        dense2 = layers.dense(inputs=relu4, units=num_actions, scope=scope, reuse=reuse, name='dense2')
+        out = layers.flatten(dense2, name='flatten1')
 
         ##############################################################
         ######################## END YOUR CODE #######################
